0fb7add4d985ab06a1d04f0b0a923d297c006c54
hydrexz <ft.hydrex@gmail.com>
2025-02-24 22:25:04 +0200

    adds session to login

--- EcoPay_backend/login.php
+++ EcoPay_backend/login.php
@@ -1,6 +1,8 @@
 <?php
 require_once 'db_connection.php';
 
+session_start(); 
+
 if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     echo "POST requests only.";
     exit;
@@ -15,12 +17,14 @@ if (empty($email) || empty($pass)) {
 }
 
 try {
-    $stmt = $pdo->prepare("SELECT * FROM Users WHERE email = ?");
+    $stmt = $pdo->prepare("SELECT id, password FROM Users WHERE email = ?"); 
     $stmt->execute([$email]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if ($user && password_verify($pass, $user["password"])) {
+        $_SESSION["user_id"] = $user["id"]; 
         echo "Login successful!";
+        
     } else {
         echo "Invalid credentials.";
     }
