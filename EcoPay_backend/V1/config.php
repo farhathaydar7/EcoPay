fb44af46fbf695cec14dcb7f284b677ae8d15ca9
hydrexz <ft.hydrex@gmail.com>
2025-03-05 19:05:06 +0200

    testing

---
+++ EcoPay_backend/V1/config.php
@@ -0,0 +1,20 @@
+<?php
+
+// Database credentials
+$host = 'localhost';
+$dbName = 'project_ecopay';
+$username = 'root';
+$password = '';
+
+// Email settings
+$adminEmail = 'haydarfarhat70pubg@gmail.com';
+$supportEmail = 'haydarfarhat70pubg@gmail.com';
+$smtpUsername = 'haydarfarhat70pubg@gmail.com';
+$smtpPassword = 'bfsmnvmzjnqqwfin';
+
+// API endpoints
+$apiBaseUrl = ''; 
+
+
+
+?>
---
+++ EcoPay_backend/V2/config.php
@@ -0,0 +1,20 @@
+<?php
+
+// Database credentials
+$host = 'localhost';
+$dbName = 'project_ecopay';
+$username = 'hr';
+$password = 'Haydar12#$5';
+
+// Email settings
+$adminEmail = 'haydarfarhat70pubg@gmail.com';
+$supportEmail = 'haydarfarhat70pubg@gmail.com';
+$smtpUsername = 'haydarfarhat70pubg@gmail.com';
+$smtpPassword = 'bfsmnvmzjnqqwfin';
+
+// API endpoints
+$apiBaseUrl = 'http://52.47.95.15'; //linkto server = Project_EcoPay
+
+
+
+?>
